504578a47d7e86288bc255f38a74dc468382621e
Maximilien Lesterlin <lesterlinpro@gmail.com>
2023-05-13 18:47:11 +0200

    Mise à jour de l'arborescence

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,4 @@
-package bataillenavale;
+
 
 import java.net.*;
 import java.io.*;
---
+++ src/lancement.sh
@@ -0,0 +1,7 @@
+javac /sae_2.03_grp_08/bataillenavale/Serveur.java ../sae_2.03_grp_08/bataillenavale/metier/*.java -encoding utf-8
+
+cd /sae_2.03_grp_08
+
+echo $(hostname)
+
+java bataillenavale.Serveur
