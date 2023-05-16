4de8ff33682e2f218d4e339dd5c3dca900b90d01
Maximilien Lesterlin <lesterlinpro@gmail.com>
2023-05-16 13:31:28 +0200

    Modificaitons

--- src/lancement.sh
+++ src/lancement.sh
@@ -1,4 +1,4 @@
-javac /sae_2.03_grp_08/bataillenavale/Serveur.java ../sae_2.03_grp_08/bataillenavale/metier/*.java -encoding utf-8
+javac /sae_2.03_grp_08/src/bataillenavale/Serveur.java ../sae_2.03_grp_08/src/bataillenavale/metier/*.java -encoding utf-8
 
 cd /sae_2.03_grp_08
 
