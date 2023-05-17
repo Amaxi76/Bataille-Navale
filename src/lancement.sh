54202718660a764a8e462d7991678cc1328ef108
Amaxi76 <115169986+Amaxi76@users.noreply.github.com>
2023-05-17 10:52:42 +0200

    Update lancement.sh

--- src/lancement.sh
+++ src/lancement.sh
@@ -1,6 +1,6 @@
 javac /sae_2.03_grp_08/src/bataillenavale/Serveur.java ../sae_2.03_grp_08/src/bataillenavale/metier/*.java -encoding utf-8
 
-cd /sae_2.03_grp_08
+cd /sae_2.03_grp_08/src
 
 echo $(hostname)
 
