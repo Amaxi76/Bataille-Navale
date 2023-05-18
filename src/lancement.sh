9a94859c940aa925ac6f05db57e5956c83dde839
Amaxi76 <115169986+Amaxi76@users.noreply.github.com>
2023-05-18 21:22:09 +0200

    Update lancement.sh

--- src/lancement.sh
+++ src/lancement.sh
@@ -1,8 +1,8 @@
-cd /sae_2.03_grp_08/src/bataillenavale
+cd sae_2.03_grp_08/src/bataillenavale
 
 javac @compile.list -encoding utf-8
 
-cd /sae_2.03_grp_08/src
+cd sae_2.03_grp_08/src
 
 echo "nom du serveur : " $(hostname)
 
